2d9041f0f4e5e0259786800249535d18a32c9c79
Sitorhy <sitorhy@gmail.com>
2019-09-09 22:31:32 +0800

    add console remove plugin

--- testbed/babel.config.js
+++ testbed/babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
-  plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]]
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { legacy: true }]
+    /*["transform-remove-console", { "exclude": [ "error", "warn"] }]*/
+  ]
 };
